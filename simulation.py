30984c39b4b81441eb3e606fe2e7be094ada3d00
Linw <49594705+limwe@users.noreply.github.com>
2024-05-31 22:16:16 +0800

    Create simulation.py

---
+++ simulation.py
@@ -0,0 +1,34 @@
+import numpy as np
+import math
+import random
+"""
+Due to the privacy of the course, please contact the author to obtain this part of the code, 
+email: llw_clever@outlook.com
+WeChat: 18500818693
+"""
+
+class Job:
+    def __init__(self, job_id, arrival_time=None, service_time=None, required_group_category=0):
+      pass
+
+
+class Dispatcher:
+    def __init__(self):
+        pass
+
+    def enqueue_job(self, job):
+        pass
+
+
+class Server:
+    def __init__(self, server_group_category):
+        pass
+
+    def complete_current_job(self):
+        pass
+
+
+class Simulator:
+  def __init__(self):
+    pass
+  
